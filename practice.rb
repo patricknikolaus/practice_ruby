8712418988e27d01a45135418a40acbdc740be2a
Patrick <pdn6991@gmail.com>
2021-06-08 01:50:48 -0400

    typo

--- practice.rb
+++ practice.rb
@@ -1,4 +1,4 @@
-1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 number = 10
 if number == 10
   p 0
