f8e535fbb4f83eaed860943a81d0abfe9b4ffa9d
Patrick <pdn6991@gmail.com>
2021-06-08 13:05:49 -0400

    finished array practice

--- practice.rb
+++ practice.rb
@@ -305,3 +305,63 @@ while number <= 40
   p number
   number += 2
 end
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+array = ["hello", "goodbye", "goodnight"]
+array << "tomorrow"
+array << "today"
+p array
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+array = ["A", "B", "C", "D"]
+array[1] = 13
+p array
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+array = [13, 22, 35, 49, 50]
+index = 0
+while index < array.length
+  p array[index]
+  index += 1
+end
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+array = [13]
+array.push(25, 66, 100)
+p array
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+array = ["hello", "goodbye", "today"]
+array[2] = array[2].upcase
+p array
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+names = ["Obi", "Jippy", "Franklin"]
+index = 0
+while index < names.length
+  p names[index]
+  index += 1
+end
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+array = ["hello", "goodbye"]
+array << "today"
+p array
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+numbers = [1, 2, 3, 4, 5]
+numbers[0] = numbers[0] * 10
+p numbers
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+numbers = [13, 66]
+index = 0
+while index < numbers.length
+  p numbers[index]
+  index += 1
+end
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+countries = ["Canada", "Germany", "Korea"]
+countries << "Denmark"
+p countries
