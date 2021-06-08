8262969907e9dd2f2fba374ed6888d6e94962441
Patrick <pdn6991@gmail.com>
2021-06-07 23:57:53 -0400

    more practice

--- practice.rb
+++ practice.rb
@@ -1,96 +1,164 @@
-# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-number = 10
-if number == 10
-  p 0
-else
-  p -1
-end
+# # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# number = 10
+# if number == 10
+#   p 0
+# else
+#   p -1
+# end
 
-# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
-number = 10
-if number < 10
-  p -1
-elsif number == 10
-  p 0
-else
-  p 1
-end
+# # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+# number = 10
+# if number < 10
+#   p -1
+# elsif number == 10
+#   p 0
+# else
+#   p 1
+# end
 
-# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-number1 = 10
-number2 = 11
-if number1 < 10 && number2 < 10
-  p 1
-else
-  p 0
-end
+# # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# number1 = 10
+# number2 = 11
+# if number1 < 10 && number2 < 10
+#   p 1
+# else
+#   p 0
+# end
 
-# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-number = 9000
-if number > 9000
-  p 1
-else
-  p -1
-end
+# # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+# number = 9000
+# if number > 9000
+#   p 1
+# else
+#   p -1
+# end
 
-# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-number = 9
-if number < 10
-  p 9
-elsif number < 20
-  p 19
-elsif number < 30
-  p 29
-else
-  p -1
-end
+# # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+# number = 9
+# if number < 10
+#   p 9
+# elsif number < 20
+#   p 19
+# elsif number < 30
+#   p 29
+# else
+#   p -1
+# end
 
-# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-number1 = 1
-number1 = 12
-if number1 > 10 || number2 > 10
-  p 100
-else
-  p -100
-end
+# # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+# number1 = 1
+# number1 = 12
+# if number1 > 10 || number2 > 10
+#   p 100
+# else
+#   p -100
+# end
 
-# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-number = -1
-if number < 0 
-  p 1776
-else
-  p 1979
-end
+# # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+# number = -1
+# if number < 0 
+#   p 1776
+# else
+#   p 1979
+# end
 
-# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-number = 99
-if number == 100
-  p 100
-elsif number == 99
-  p 99
-else
-  p 0
-end
+# # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+# number = 99
+# if number == 100
+#   p 100
+# elsif number == 99
+#   p 99
+# else
+#   p 0
+# end
 
-# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-number1 = -1
-number2 = 1
-if number1 < 0 && number2 > 0
-  p 1
-else
-  p 0
-end
+# # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+# number1 = -1
+# number2 = 1
+# if number1 < 0 && number2 > 0
+#   p 1
+# else
+#   p 0
+# end
 
-# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
-number = 19
-if number > 80 
-  p 5
-elsif number > 60
-  p 4
-elsif number > 40
-  p 3
-elsif number > 20
-  p 2
-else
-  p 1
-end
+# # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+# number = 19
+# if number > 80 
+#   p 5
+# elsif number > 60
+#   p 4
+# elsif number > 40
+#   p 3
+# elsif number > 20
+#   p 2
+# else
+#   p 1
+# end
+
+# # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+# first = "Patrick"
+# last = "Nikolaus"
+# p first + " " + last
+
+# # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+# first = "Patrick"
+# last = "Nikolaus"
+
+# p "#{first} #{last}"
+
+# # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+# print "Please enter a word: "
+# word = gets.chomp
+# if word == "Marco"
+#   p "Polo"
+# end
+
+# # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+# color1 = "green"
+# color2 = "purple"
+# color3 = "black"
+
+# p "My favorite color is " + color1 + ", my second favorite color is " + color2 + ", and my favorite color to wear is " + color3 + "."
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+# color1 = "green"
+# color2 = "purple"
+# color3 = "black"
+
+# p "My favorite color is #{color1}, my second favorite color is #{color2}, and my favorite color to wear is #{color3}."
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+# print "Please enter a name: "
+# name = gets.chomp
+# if name != "Santa"
+#   p "You're not santa."
+# end
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+# book = "Holes"
+# author = "Louis Sachar"
+
+# p "When I was a kid my favorite book was " + book + " by " + author + "."
+
+
+# # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+# book = "Holes"
+# author = "Louis Sachar"
+
+# p "When I was a kid my favorite book was #{book} by #{author}."
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+# print "Enter the password: "
+# password = gets.chomp
+# if password == "Joshua"
+#   p "Shall we play a game?"
+# else
+#   p "Access denied"
+# end
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+city1 = "Columbus"
+city2 = "Chicago"
+city3 = "L.A."
+
+
