f709d9a5c5a11c5466e2b13406c025fe0a4b2ce8
Patrick <pdn6991@gmail.com>
2021-06-07 23:21:38 -0400

    more practice

--- practice.rb
+++ practice.rb
@@ -17,6 +17,12 @@ else
 end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+number = 10,2
+if number[0] < 10 && number[1] < 10
+  p 1
+else
+  p 0
+end
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
