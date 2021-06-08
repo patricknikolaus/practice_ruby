550cfda0850199b8c74e7c0ea241ead9eaf4e520
Patrick <pdn6991@gmail.com>
2021-06-07 21:56:37 -0400

    added dating and marriage conditionals

--- count.rb
+++ count.rb
@@ -1,6 +1,14 @@
-count = 0
+sam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+sally = ["French", "English", "Spanish", "Korean", "Russian", "German"]
 
-while count <= 10
-  p count
-  count += 1
+if sam.length > 10 && sally.length > 5
+  p "They should date."
+else
+  p "They shouldn't date."
+end
+
+if sally.include?("French")
+  p "They should get married."
+else
+  p "They should not get married."
 end
