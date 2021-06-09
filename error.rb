366050ecfc2a7f19436855f94f19258c7a7bddbd
Patrick <pdn6991@gmail.com>
2021-06-09 12:32:41 -0400

    practice commit

--- error.rb
+++ error.rb
@@ -32,15 +32,16 @@
 # end
 
 class Person
-  def initial(firstname, lastname)
+  def initialize(firstname, lastname)
     @first_name = firstname
-    @last_name == lname
+    @last_name == lastname
   end
 
   def to_s
-    @last_name + ” ” + @first_name
+    "#{@last_name} #{@first_name}"
   end
+
 end
 tj = Person.new("Thomas", "Jefferson")
-puts person.fname
-put tj
+puts person.firstname
+puts tj
