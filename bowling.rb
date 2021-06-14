ba60aa99c691e25c1a964912cd8edb7e796f7da2
Patrick <pdn6991@gmail.com>
2021-06-14 16:22:50 -0400

    finished computing single values in arrays skills

--- bowling.rb
+++ bowling.rb
@@ -1,51 +1,32 @@
-# Rolls = []
-# frame = []
+#uses classes instead 
+#class for the rolls
+#take spares into consideration
+#take strikes into consideration
+#take last frame into consideration
+#class the calculates the score
 
-# until scores.length == 10
-#   print "Enter 1st roll score: "
-#   score1 = gets.chomp.to_i
-#   if score1 == 10
-#     frame << score1
-#   elsif score1 < 10
-#     print "Enter 2nd roll score: "
-#     score2 = gets.chomp.to_i
-#     frame << score1
-#     frame << score2
-#   end
-#   scores << frame
-#   frame = []
-# end 
-# p scores
-
-class Frame
-  attr_reader :roll1, :roll2
-
-  def initialize(rolls_input)
-    @roll1 = rolls_input[:roll1]
-    @roll2 = rolls_input[:roll2]
+class Game
+  def initialize
+    @score_board = []
   end
-
-  def rolled 
+  
+  def frame(num1, num2)
+    @roll1 = num1
+    @roll2 = num2
     if @roll1 == 10
-      @roll1 = 'X'
-    elsif @roll1 + @roll2 == 10
-      @roll1 = '/'
-    else
-      @roll1 = @roll1 + @roll2
+      @frame = [@roll1]
+      @score_board << @frame
+    elsif @frame = [@roll1, @roll2]
+      @score_board << @frame
     end
   end
+
 end
 
-class Score < Frame
-  def score_sheet
-    @score_sheet = []
-  end
-end
+class Score < Game
   
-frame1 = Frame.new({roll1: 10})
-frame2 = Frame.new({roll1: 4, roll2: 5})
-frame3 = Frame.new(roll1: 4, roll2: 6)
+end
 
-p frame1.rolled
-p frame2.rolled
-p frame3.rolled
+player1 = Game.new
+
+p player1.frame(1,2)
--- practice.rb
+++ practice.rb
@@ -576,3 +576,105 @@ strings = numbers.map do |string|
   string.to_s
 end
 p strings
+
+#  1. Start with an array of numbers and compute the sum of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 26.
+array = [5, 10, 8, 3]
+sum = 0
+array.each do |number|
+  sum += number
+end
+p sum
+
+#  2. Start with an array of strings and combine them all into a single string.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "volleyballbasketballbadminton".
+array = ["volleyball", "basketball", "badminton"]
+string = ""
+array.each do |sport|
+  string += sport
+end
+p string
+
+#  3. Start with an array of hashes and compute the sum of the prices (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes 105.
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+price = 0
+array.each do |item|
+  price += item[:price]
+end
+p price
+
+
+#  4. Start with an array of numbers and compute the the minumum number.
+#     For example, [5, 10, 8, 3, 9] becomes 3.
+array = [5, 10, 8, 3, 9]
+smallest = array[0]
+array.each do |number|
+  if number < smallest 
+    smallest = number
+  end
+end
+p smallest
+
+#  5. Start with an array of strings and compute the total length of all the strings.
+#     For example, ["volleyball", "basketball", "badminton"] becomes 29.
+array = ["volleyball", "basketball", "badminton"]
+total_length = 0
+array.each do |sport|
+  total_length += sport.length
+end
+p total_length
+
+#  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+lowest = array[0]
+array.each do |item|
+  if item[:price] < lowest[:price]
+    lowest = item
+  end
+end
+p lowest
+
+#  7. Start with an array of numbers and compute product of all the numbers.
+#     For example, [5, 10, 8, 3] becomes 1200.
+array = [5, 10, 8, 3]
+product = 1
+array.each do |number|
+  product *= number
+end
+p product
+
+
+#  8. Start with an array of strings and combine them all into a single string, separated by dashes.
+#     For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
+array = ["volleyball", "basketball", "badminton"]
+string = "-"
+array.each do |sport|
+  string += "#{sport}-"
+end
+p string
+
+
+#  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
+#     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
+array = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+shortest = array[0]
+array.each do |item|
+  if item[:name].length < shortest[:name].length
+    shortest = item
+  end
+end
+p shortest
+
+
+# 10. Start with an array of numbers and compute the maximum number.
+#     For example, [5, 10, 8, 3] becomes 10.
+array = [5, 10, 8, 3]
+largest = 0
+array.each do |number|
+  if number > largest
+    largest = number
+  end
+end
+p largest
